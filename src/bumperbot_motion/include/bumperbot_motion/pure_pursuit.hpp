beb78b00ddb92235f27c01d95bd4ba468c53b129
Robert Cowher <bobcowher@gmail.com>
2025-07-14 21:32:53 -0500

    Pure Pursuit Algorithm

---
+++ src/bumperbot_motion/include/bumperbot_motion/pure_pursuit.hpp
@@ -0,0 +1,48 @@
+#include "geometry_msgs/msg/pose.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include <memory>
+#include <rclcpp/node.hpp>
+#include <rclcpp/publisher.hpp>
+#include <rclcpp/subscription.hpp>
+#include "nav_msgs/msg/path.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+#include "geometry_msgs/msg/pose_stamped.hpp"
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/transform_listener.h"
+
+namespace bumperbot_motion
+{
+
+class PurePursuit : public rclcpp::Node
+{
+
+public:
+	PurePursuit();
+
+private:
+	rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr path_sub_;
+	rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_pub_;
+	rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr carrot_pose_pub_;
+	std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
+	std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+	rclcpp::TimerBase::SharedPtr control_loop_;
+	
+	double look_ahead_distance_;
+	double max_linear_velocity_;
+	double max_angular_velocity_;
+	nav_msgs::msg::Path global_plan_;
+
+	void controlLoop();
+	void pathCallback(const nav_msgs::msg::Path::SharedPtr path);
+
+	bool transformPlan(const std::string & frame);
+
+	geometry_msgs::msg::PoseStamped getCarrotPose(const geometry_msgs::msg::PoseStamped & robot_pose);
+
+	double getCurvature(const geometry_msgs::msg::Pose & carrot_pose);
+			
+// End of PurePursuit Class
+};
+
+// End of namespace
+}
